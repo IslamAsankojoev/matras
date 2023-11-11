3546c396810c2f3a3840f682bff4528ddd646d8a
IslamAsankojoev <isla.asankojoev@mail.ru>
2023-11-11 16:23:20 +0600

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,232 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
+import { Button, Col, Grid, Row, Space } from 'antd'
+import React from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
+const whatsapp = 'https://wa.me/+996555555779'
+const montserrat = Montserrat({ subsets: ['latin'] })
+
+const Container = ({ children }: { children: any }) => (
+  <div
+    style={{
+      maxWidth: '1200px',
+      margin: '0 auto',
+      width: '100%',
+      padding: '0 15px',
+    }}
+  >
+    {children}
+  </div>
+)
 
 export default function Home() {
+  const [scrollY, setScrollY] = React.useState(0)
+
+  React.useEffect(() => {
+    const handleScroll = () => setScrollY(window.scrollY)
+    window.addEventListener('scroll', handleScroll, { passive: true })
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between ${montserrat.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <div className="overlay fixed w-full h-screen bg-[rgba(0,0,0,.0)]"></div>
+      <div
+        className="h-screen"
+        style={{
+          backgroundImage: 'url(/firstBG.webp)',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          display: 'flex',
+          justifyContent: 'center',
+          padding: '0 10%',
+          flexDirection: 'column',
+          zIndex: 10,
+        }}
+      >
+        <section
+          id="header"
+          className="fixed top-0 left-0 w-full flex justify-between py-6 transition"
+          style={{
+            background: scrollY > 0 ? 'rgba(0,0,0,0.9)' : 'rgba(0,0,0,0.3)',
+            padding: scrollY > 0 ? '15px 0' : '20px 0',
+          }}
+        >
+          <Container>
+            <div className="flex justify-between w-full">
+              <div className="flex items-center justify-center">
+                <Image src="/vercel.svg" width={100} height={100} alt="logo" className="invert" />
+              </div>
+              <div className="flex items-center justify-center">
+                <Button href={whatsapp} target='_blank' type="primary" size="middle" className="bg-blue-600">
+                  Свяжитесь с нами
+                </Button>
+              </div>
+            </div>
+          </Container>
+        </section>
+        <Row>
+          <Col md={12} sm={24}>
+            <Space size="large" direction="vertical">
+              <h2 className={`${montserrat.className} text-xl md:text-3xl text-white`}>
+                Купите качественные матрасы и груши для вашего спортзала
+              </h2>
+              <p className={`${montserrat.className} text-sm md:text-lg text-white`}>
+                Добро пожаловать в наш магазин спортивного оборудования, где вы найдете широкий
+                ассортимент матрасов и груш для спортзала. Наши продукты обеспечат вам комфорт и
+                безопасность во время тренировок. Мы гордимся предоставлением продукции высокого
+                качества по доступным ценам.
+              </p>
+              <Button href={whatsapp} target='_blank' type="primary" size="large" className="bg-blue-600">
+                Свяжитесь с нами
+              </Button>
+            </Space>
+          </Col>
+          <Col md={12} sm={24}></Col>
+        </Row>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {/* block1 */}
+      <Container>
+        <section className="py-40">
+          <Row>
+            <Col md={12} sm={24}>
+              <Space className="p-4">
+                <Image src="/155-tatami-large.png" width={500} height={500} alt="logo" />
+              </Space>
+            </Col>
+            <Col md={12} sm={24}>
+              <Space className="p-4" direction="vertical" size={20}>
+                <h2 className={`${montserrat.className} text-2xl md:text-3xl`}>
+                  Матрасы Татами Пазлы: Комфорт и Защита
+                </h2>
+                <p className={`${montserrat.className} text-base md:text-lg`}>
+                  Наши матрасы Татами Пазлы - это идеальное решение для спортзалов и тренажерных
+                  помещений. Они обеспечивают отличное смягчение при падениях и ударах, что делает
+                  их идеальным выбором для занятий боевыми искусствами, гимнастикой, йогой и многими
+                  другими видами физической активности. Наши матрасы легко соединяются в единое
+                  целое и удобны в уходе. Выберите качество и надежность с нашими матрасами Татами
+                  Пазлы.
+                </p>
+              </Space>
+            </Col>
+          </Row>
+        </section>
+      </Container>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <section id="our-prems" className="w-full p-10 bg-gray-800">
+        <Container>
+          <Row className='gap-8 md:gap-0'>
+            <Col md={6} sm={24}>
+              <Space direction="vertical" align="center">
+                <Image src="/quality.png" alt="quality" width={80} height={80} className="invert" />
+                <h4 className="text-sm text-white">Высокое качество матрасов</h4>
+                <p className="text-xs text-white text-center">
+                  Наши матрасы изготовлены из прочных материалов, обеспечивая долгий срок службы и
+                  максимальный комфорт.
+                </p>
+              </Space>
+            </Col>
+            <Col md={6} sm={24}>
+              <Space direction="vertical" align="center">
+                <Image
+                  src="/in-stock.png"
+                  alt="quality"
+                  width={80}
+                  height={80}
+                  className="invert"
+                />
+                <h4 className="text-sm text-white">Широкий выбор груш для спорта</h4>
+                <p className="text-xs text-white text-center">
+                  Мы предлагаем разнообразие груш для тренировок, подходящих для разных видов спорта
+                  и уровней подготовки.
+                </p>
+              </Space>
+            </Col>
+            <Col md={6} sm={24}>
+              <Space direction="vertical" align="center">
+                <Image
+                  src="/shipped.png"
+                  alt="shipping"
+                  width={80}
+                  height={80}
+                  className="invert"
+                />
+                <h4 className="text-sm text-white ">Быстрая доставка и установка</h4>
+                <p className="text-xs text-white text-center">
+                  Мы гарантируем быструю доставку и помощь в установке вашего спортивного
+                  оборудования для немедленного использования.
+                </p>
+              </Space>
+            </Col>
+            <Col md={6} sm={24}>
+              <Space direction="vertical" align="center">
+                <Image src="/businessman.png" alt="quality" width={80} height={80} className="invert" />
+                <h4 className="text-sm text-white">Профессиональные консультации</h4>
+                <p className="text-xs text-white text-center">
+                  Наши эксперты готовы предоставить вам советы и рекомендации по выбору и уходу за
+                  вашим оборудованием.
+                </p>
+              </Space>
+            </Col>
+          </Row>
+        </Container>
+      </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      {/* block2 */}
+      <Container>
+        <section className="py-40">
+          <Row>
+            <Col md={12} sm={24}>
+              <Space className="p-4" direction="vertical" size={20}>
+                <h2 className={`${montserrat.className} text-2xl md:text-3xl`}>
+                  Груши для Тренировок: Сила и Устойчивость
+                </h2>
+                <p className={`${montserrat.className} text-base md:text-lg`}>
+                  Если вы ищете эффективное оборудование для упражнений и тренировок, наши груши -
+                  идеальный выбор. Груши спроектированы для укрепления мышц и улучшения координации
+                  движений. Они подходят для бокса, ударных тренировок и многих других видов спорта.
+                  Все груши в нашем ассортименте изготовлены из высококачествных материалов,
+                  обеспечивая долгий срок службы.
+                </p>
+              </Space>
+            </Col>
+            <Col md={12} sm={24}>
+              <Space className="p-4">
+                <Image src="/punch-bag.jpg" width={500} height={500} alt="logo" />
+              </Space>
+            </Col>
+          </Row>
+        </section>
+      </Container>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+      {/* footer */}
+      <section id="footer" className="w-full p-10 bg-gray-800">
+        <Container>
+          <Space className="flex justify-between md:flex-row flex-col gap-4 ">
+            <div className="flex items-center justify-center">
+              <Image src="/vercel.svg" width={100} height={100} alt="logo" className="invert" />
+            </div>
+            <div className="flex items-center justify-center text-white text-center">
+              @2023 by Islam & Beknazar. Защищено законом об авторских правах.
+            </div>
+            <div className="flex items-center justify-center">
+              <Button href={whatsapp} target='_blank' type="primary" size="middle" className="bg-blue-600">
+                Свяжитесь с нами
+              </Button>
+            </div>
+          </Space>
+        </Container>
+      </section>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <Button href={whatsapp} type="link" className='whatsapp-button fixed bottom-10 right-8 z-50 w-14 h-14 rounded-full p-0 m-0 hover:scale-110 active:scale-100'>
+        <Image src="/whatsapp.png" alt='whatsapp' width={50} height={50}/>
+      </Button>
+
+
     </main>
   )
 }
